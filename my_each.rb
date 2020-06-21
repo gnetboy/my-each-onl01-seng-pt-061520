dfb06d21e138ebb10de12b12a366da851afe3644
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-21 03:49:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
-def my_each()           
+def my_each(array)           
   counter = i 
   
   while counter < array.length
   yield array |counter|
   i +=1
 end
+    array
+end
+
+my_each(collection) do |counter|
+  puts counter
 end
